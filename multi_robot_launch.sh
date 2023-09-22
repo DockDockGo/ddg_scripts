738b1fa349423b0af15e00d4ad689643fe6a18ea
VineetTambe <tambevineet@gmail.com>
2023-09-22 17:07:57 -0400

    Updated scripts for cbs

--- multi_robot_launch.sh
+++ multi_robot_launch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 # gnome-termial
-cd ~/mp_400_workspace && . install/setup.bash
+cd ../../ws && . install/setup.bash
 # Kill previous session
 tmux kill-session
 
@@ -20,7 +19,9 @@ tmux selectp -t 0    # go back to the first pane
 tmux splitw -h -p 33 # split it into two halves
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -h -p 40 # split it into two halves
-# tmux splitw -h    -p 33 # split it into two halves
+
+tmux selectp -t 3    # go back to the first pane
+tmux splitw -h -p 50 # split it into two halves
 
 # tmux selectp -t 3    # go back to the first pane
 # tmux splitw -h -p 33 # split it into two halves
@@ -48,14 +49,25 @@ tmux select-pane -t 2
 tmux send-keys "export MAP_NAME=$map" Enter
 tmux send-keys "export MY_ROBOT=mp_400" Enter
 tmux send-keys "export Number_of_Robots=$num_robots" Enter
-tmux send-keys "sleep 7 && ros2 launch neo_nav2_bringup rviz_launch.py rviz_config:=install/neo_nav2_bringup/share/neo_nav2_bringup/rviz/svd_demo_final.rviz" Enter
+# tmux send-keys "sleep 7 && ros2 launch neo_nav2_bringup rviz_launch.py rviz_config:=install/neo_nav2_bringup/share/neo_nav2_bringup/rviz/svd_demo_final.rviz" Enter
+tmux send-keys "sleep 7 && ros2 launch neo_nav2_bringup rviz_launch.py rviz_config:=src/neo_nav2_bringup/rviz/svd_demo_final.rviz" Enter
 
-# Run multi-robot commander
-# tmux select-pane -t 3
-# tmux send-keys "export MAP_NAME=\"$map\"" Enter
-# tmux send-keys "export MY_ROBOT=mp_400" Enter
-# tmux send-keys "export Number_of_Robots=\"$num_robots\"" Enter
+
+Run multi-robot commander
+tmux select-pane -t 3
+tmux send-keys "export MAP_NAME=\"$map\"" Enter
+tmux send-keys "export MY_ROBOT=mp_400" Enter
+tmux send-keys "export Number_of_Robots=\"$num_robots\"" Enter
+tmux send-keys "sleep 20 && ros2 launch ddg_multi_robot_planner multi_robot_planner.launch.py" Enter 
 # tmux send-keys "sleep 60 && ros2 run multi_navigator multi_commander" Enter 
 
+
+Run multi-robot commander
+tmux select-pane -t 4
+tmux send-keys "export MAP_NAME=\"$map\"" Enter
+tmux send-keys "export MY_ROBOT=mp_400" Enter
+tmux send-keys "export Number_of_Robots=\"$num_robots\"" Enter
+tmux send-keys "sleep 60 && ros2 run multi_navigator multi_commander" Enter 
+
 # Attach to the tmux session
-tmux -2 attach-session -t $session_name -c ~/mp_400_workspace
+tmux -2 attach-session -t $session_name -c /ws
